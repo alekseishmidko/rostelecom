291b99bcdfb021a3030d960db128de55c42ad539
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-03-16 22:24:59 +0300

    16.03.24-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import './globalStyles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Layout from '@/components/layouts/Layout'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   )
 }
